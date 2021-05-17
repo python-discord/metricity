cf5e4c1859797d45d620f8b6dd50698c16812373
vcokltfre <vcokltfre@gmail.com>
2021-05-17 01:25:18 +0100

    fix: remove unused Union import

--- metricity/config.py
+++ metricity/config.py
@@ -2,7 +2,7 @@
 import logging
 from os import environ
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple
 
 import toml
 from deepmerge import Merger
