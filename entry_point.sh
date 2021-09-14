245281e8f4eefe41ce684db972315c1ab3728f94
Chris Lovering <chris.lovering.95@gmail.com>
2021-09-14 12:17:21 +0100

    Detect and copy config files passed by bot compose

--- entry_point.sh
+++ entry_point.sh
@@ -3,6 +3,11 @@ set -e
 python create_metricity_db.py
 alembic upgrade head
 
+if [ -e /tmp/bot/metricity-config.toml ]; then
+    echo "Detected metricity running in bot context, copying config." 
+    cp --force /tmp/bot/metricity-config.toml ./config.toml
+fi
+
 shopt -s nocasematch
 if [ "${USE_METRICITY:-true}" = "true" ]; then
     poetry run start
