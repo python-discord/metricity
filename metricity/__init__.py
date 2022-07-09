d3470a98c964152530933217cf58a6dca2bf7d94
Chris Lovering <chris.lovering.95@gmail.com>
2022-07-09 21:49:28 +0100

    Migrate metricity to use BotBase from botcore

--- metricity/__init__.py
+++ metricity/__init__.py
@@ -1,12 +1,20 @@
 """Metric collection for the Python Discord server."""
 
+import asyncio
 import logging
+import os
+from typing import TYPE_CHECKING
+
 
 import coloredlogs
+from botcore.utils import apply_monkey_patches
 
 from metricity.config import PythonConfig
 
-__version__ = "1.3.0"
+if TYPE_CHECKING:
+    from metricity.bot import Bot
+
+__version__ = "1.4.0"
 
 # Set root log level
 logging.basicConfig(level=PythonConfig.log_level)
@@ -18,3 +26,11 @@ logging.getLogger("discord.client").setLevel(PythonConfig.discord_log_level)
 # Gino has an obnoxiously loud log for all queries executed, not great when inserting
 # tens of thousands of users, so we can disable that (it's just a SQLAlchemy logger)
 logging.getLogger("gino.engine._SAEngine").setLevel(logging.WARNING)
+
+# On Windows, the selector event loop is required for aiodns.
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+apply_monkey_patches()
+
+instance: "Bot" = None  # Global Bot instance.
--- metricity/__main__.py
+++ metricity/__main__.py
@@ -1,9 +1,49 @@
 """Entry point for the Metricity application."""
 
-from metricity.bot import bot
+import asyncio
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+import metricity
+from metricity.bot import Bot
 from metricity.config import BotConfig
+
+
+async def main() -> None:
+    """Entry async method for starting the bot."""
+    intents = discord.Intents(
+        guilds=True,
+        members=True,
+        bans=False,
+        emojis=False,
+        integrations=False,
+        webhooks=False,
+        invites=False,
+        voice_states=False,
+        presences=False,
+        messages=True,
+        reactions=False,
+        typing=False
+    )
+
+    async with aiohttp.ClientSession() as session:
+        metricity.instance = Bot(
+            guild_id=BotConfig.guild_id,
+            http_session=session,
+            command_prefix=commands.when_mentioned,
+            activity=discord.Game(f"Metricity {metricity.__version__}"),
+            intents=intents,
+            max_messages=None,
+            allowed_mentions=None,
+            allowed_roles=None,
+            help_command=None,
+        )
+        async with metricity.instance as _bot:
+            await _bot.start(BotConfig.token)
 
 
 def start() -> None:
     """Start the Metricity application."""
-    bot.run(BotConfig.token)
+    asyncio.run(main())
